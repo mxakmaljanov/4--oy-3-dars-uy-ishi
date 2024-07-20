5305f518db476d126d7241a95b30ff2a4ecc3f0c
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-07-20 18:16:44 +0500

    3--dars

---
+++ index.js
@@ -0,0 +1,161 @@
+// 1 - masala
+// let age = + prompt("Yoshingizni kiriting");
+// let res;
+// if (age == 18) {
+//     res = "Siz katta yoshdagisiz"
+// } else {
+//     res = "Siz hali yoshsiz"
+// }
+// console.log(res);
+
+// 2 - masala
+
+// let num = + prompt(`Son kiriting`);
+// let res;
+// if (num % 2 == 0) {
+//     res = 'Juft son'
+// }
+// if (num % 2 == 1) {
+//     res = 'Toq son'
+// }
+// console.log(res);
+
+// 3 - masala
+
+// let num = + prompt('Son kiriting');
+// let res;
+// if (num > 0) {
+//     res = "Musbat"
+// }
+// if (num < 0) {
+//     res = 'Manfiy'
+// }
+// console.log(res);
+
+// 4 - masala
+
+// let Harorat = + prompt("Haroratni raqamlarda kiriting");
+// let res;
+// if (Harorat > 20) {
+//     res = "Yengil kiying"
+// } else {
+//     res = "Qalin kiying"
+// }
+// console.log(res);
+
+// 5 - masala
+
+// let narx = + prompt("Narxni summada kiriting");
+// let res;
+// if (narx > 100) {
+//     res = 'Chegirma qollanildi'
+// } else {
+//     res = "Chegirma mavjud emas"
+// }
+// console.log(res);
+// 6 - masala
+// let harorat = + prompt("Haroratni raqamlarda kiriting");
+// let res;
+// harorat > 25 ? res = 'Issiq' : res = 'Sovuq';
+// console.log(res);
+// 7 - masala
+// let ball = + prompt("Ballni kiriting");
+// let res;
+// ball > 50 ? res = 'Otdi' : res = 'Yiqildi';
+// console.log(res);
+// 8 - masala
+// let num = + prompt("Sonni kiriting");
+// let res;
+// num > 0 ? res = 'Musbat' : res ;'Manfiy';
+// console.log(res);
+// 9 - masala
+// for(let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+// 10 - masala
+// for(let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+// 11- masala
+//let num= +prompt("kiriting")
+// for(let i = 1; i <= 100; i += 2) {
+//     console.log(i);
+// }
+// 12- masala
+// for(let i = 1; i <= 100; i++) {
+//     if (i % 2 == 1) {
+//         console.log(i);
+//     }
+// }
+// 13- masala
+// let sum = 0;
+// for(let i = 1; i <= 10; i++) {
+//   sum = sum + i;
+// }
+// console.log(sum);
+// 14- masala
+// let p = 1;
+// for(let i = 1; i <= 10; i++) {
+//     p = p * i;
+// }
+// console.log(p);
+// 15- masala
+// let son = + prompt('Son kiriting');
+// let faktorial = 1;
+// for(let i = 1; i <= son; i++) {
+//     faktorial *= 1;
+// }
+// console.log(faktorial);
+// 16- masala
+// for(let i = 1; i <= 50; i++) {
+//     let kvadrat = i * i;
+//     console.log(kvadrat);
+// 17- masala
+// for(let i = 1; i <= 20; i++) {
+//     let kub = i * i * i;
+//     console.log(kub);
+// }
+// 18 - masala
+// let sum = 0;
+// for(let i = 1; i <= 100; i++) {
+//     sum += i;
+//     console.log(sum);
+// }
+// 19 - masala
+// let str = ''
+// for(let i = 1; i <= 5; i++) {
+//     str += 'Salom';
+//     console.log(str);
+// }
+// 20 - masala
+// let num = +prompt("Son kiriting");
+// for(let i = 1; i <= num; i++) {
+//     let kvadrat = i * i;
+//     console.log(kvadrat);
+// }
+// 21- masala
+
+// let A = +prompt("A sonini kiriting");
+// let B = +prompt("B sonini kiriting");
+// let res;
+// if (A % 2 == 1 && B % 2 == 1 || A % 2 == 0 && B % 2 == 0) {
+//     res = true;
+// } else {
+//     res = false;
+// }
+// console.log(res);
+
+// 22- masala
+
+// let A = + prompt("A sonini kiriting");
+// let B = + prompt('B sonini kiriting');
+// let C = + prompt(`C sonini kiriting`);
+// let res;
+// if (A > 0 && B > 0 && C > 0) {
+//     res = 'Rost';
+// } else {
+//     res = 'Yolgon'
+// }
+// console.log(res);
+
+// 23 - masala
